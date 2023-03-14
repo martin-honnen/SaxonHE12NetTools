2367c85ecd9014cbbfdc7912851c8dc1e2451f4b
Martin Honnen <martin.honnen@gmx.de>
2023-03-14 16:10:43 +0100

    Projektdateien hinzufügen.

---
+++ SaxonHE12NetXslt/Transform.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using net.sf.saxon;
+
+
+namespace net.liberty_development.saxontools.SaxonXslt;
+
+public class Xslt
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"SaxonHENetXslt 12.1.0 on .NET {Environment.Version} {Environment.OSVersion}");
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        //ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("org.xmlresolver.xmlresolver"));
+        //ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("org.xmlresolver.xmlresolver_data"));
+
+        Transform.main(args);
+
+        stopWatch.Stop();
+
+        Console.WriteLine($"Elapsed time: {stopWatch.Elapsed}");
+    }
+}
