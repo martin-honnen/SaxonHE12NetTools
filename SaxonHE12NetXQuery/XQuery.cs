94898eda40ae62eef2302302ca9224633877eaf3
Martin Honnen <martin.honnen@gmx.de>
2023-03-14 16:38:48 +0100

    Prepare Saxon XQuery tool

---
+++ SaxonHE12NetXQuery/XQuery.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using net.sf.saxon;
+
+
+namespace net.liberty_development.saxontools.SaxonXQuery;
+public class XQuery
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"SaxonHE12NetXQuery 12.1.0 on .NET {Environment.Version} {Environment.OSVersion}");
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        //ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("org.xmlresolver.xmlresolver"));
+        //ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("org.xmlresolver.xmlresolver_data"));
+
+        Query.main(args);
+
+        stopWatch.Stop();
+
+        Console.WriteLine($"Elapsed time: {stopWatch.Elapsed}");
+    }
+}
