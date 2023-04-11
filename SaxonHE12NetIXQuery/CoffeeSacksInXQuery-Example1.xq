4728cabdaebc11062f8da884f5d57fe6897e79ea
Martin Honnen <martin.honnen@gmx.de>
2023-04-11 17:57:41 +0200

    Adding CoffeeSacks library project and Invisible XML XQuery console app

---
+++ SaxonHE12NetIXQuery/CoffeeSacksInXQuery-Example1.xq
@@ -0,0 +1,15 @@
+declare namespace cs = "http://nineml.com/ns/coffeesacks";
+
+declare namespace output = "http://www.w3.org/2010/xslt-xquery-serialization";
+
+declare option output:method 'xml';
+declare option output:indent 'yes';
+
+declare variable $input-date as xs:string external := '11 April 2023';
+
+<invisible-xml-test>
+{
+let $parser := cs:load-grammar('date.ixml')
+return $parser($input-date)
+}
+</invisible-xml-test>
---
+++ SaxonHE12NetIXQuery/InvisibleXmlXQuery.cs
@@ -0,0 +1,34 @@
+﻿using net.sf.saxon;
+using org.nineml.coffeesacks;
+using System.Diagnostics;
+
+namespace SaxonHE12NetIXQuery;
+
+public class InvisibleXmlXQuery : Query
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"SaxonHENetIXQuery 12.1.0 on .NET {Environment.Version} {Environment.OSVersion}");
+
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        //ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("org.xmlresolver.xmlresolver"));
+        //ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("org.xmlresolver.xmlresolver_data"));
+
+        var invisbleXmlXQuery = new InvisibleXmlXQuery();
+
+        invisbleXmlXQuery.doQuery(args, "IXQuery");
+
+        stopWatch.Stop();
+
+        Console.WriteLine($"Elapsed time: {stopWatch.Elapsed}");
+    }
+
+    protected override void initializeConfiguration(Configuration config)
+    {
+        var coffeeSacksRegister = new RegisterCoffeeSacks();
+
+        coffeeSacksRegister.initialize(config);
+    }
+}
