bec7ce57e51f219ec4a3c57b71f56634adc6bcda
Martin Honnen <martin.honnen@gmx.de>
2024-08-15 12:00:39 +0200

    Test prerelease of IKVM 8.10 which uses latest Java 8 JDK

--- SaxonHE12NetIXQuery/InvisibleXmlXQuery.cs
+++ SaxonHE12NetIXQuery/InvisibleXmlXQuery.cs
@@ -9,7 +9,7 @@ public class InvisibleXmlXQuery : Query
 {
     static void Main(string[] args)
     {
-        Console.WriteLine($"SaxonHENetIXQuery 12.5.9.1 on .NET {Environment.Version} {Environment.OSVersion}");
+        Console.WriteLine($"SaxonHENetIXQuery 12.5.9.2 on .NET {Environment.Version} {Environment.OSVersion}");
 
         var stopWatch = new Stopwatch();
         stopWatch.Start();
